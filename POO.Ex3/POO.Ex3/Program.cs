9e14883c30811b0349807abf6abe215e64883a42
CB3038963 <ferraz.joana@aluno.ifsp.edu.br>
2025-08-25 20:57:32 -0300

    Add files via upload

---
+++ POO.Ex3/POO.Ex3/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POO.Ex3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Retangulo retangulo = new Retangulo(10);
+            retangulo.Calcular();
+            Console.WriteLine(retangulo.GetArea());
+        }
+    }
+}
---
+++ POO.Ex3/POO.Ex3/Retangulo.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POO.Ex3
+{
+    internal class Retangulo
+    {
+        int d , area;
+
+        //Construtores
+        public Retangulo()
+        { this.d = 0;  this.area=0;}
+
+        public Retangulo(int d)
+        {this.d = d;}
+
+        //Set void
+        public void SetDiagonal(int d)
+        { this.d = d; }
+        //Get
+        public int GetArea()
+        {return this.area;}
+
+        public int GetDiagonal()
+        { return this.d; }
+
+        //Calcular
+        public void Calcular()
+        {this.area = this.d * this.d / 2;}
+
+
+
+    }
+}
