e43597c3aa00cb93db3e61ba3ff35459eca9f274
CB3038963 <ferraz.joana@aluno.ifsp.edu.br>
2025-08-25 20:45:18 -0300

    Add files via upload

---
+++ POO.Ex2/POO.Ex2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POO.Ex2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Retangulo retangulo = new Retangulo(5);
+            retangulo.Calcular();
+            Console.WriteLine(retangulo.GetArea());
+        }
+    }
+}
---
+++ POO.Ex2/POO.Ex2/Retangulo.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POO.Ex2
+{
+    internal class Retangulo
+    {
+        int aresta, area;
+
+        //Construtores
+        public Retangulo()
+        {this.aresta = 0;this.area = 0;}
+
+        public Retangulo(int aresta)
+        {this.aresta = aresta;}
+
+        //Set void
+        public void SetAresta(int aresta)
+        {this.aresta = aresta;}
+
+        //Get int
+        public int GetArea()
+        {return this.area;}
+        public int GetAresta()
+        {return this.aresta;}
+
+        //Calcular void
+        public void Calcular()
+        { this.area = this.aresta * this.aresta; }
+
+    }
+}
