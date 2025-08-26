5cf8b4755eceae19970230399f853fffa8423c39
CB3038963 <ferraz.joana@aluno.ifsp.edu.br>
2025-08-25 21:07:08 -0300

    Add files via upload

---
+++ POO.Ex1/POO.Ex1/Calcular.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POO.Ex1
+{
+    internal class Calcular
+    {
+        int b, h, a;
+
+        //Construtores
+        public Calcular()
+        { this.b = 0; this.h = 0;}
+
+        public Calcular(int b, int h)
+        {this.b = b;this.h = h;}
+
+        //Set void
+        public void SetBase(int b)
+        { this.b = b; }
+        public void SetAltura(int h)
+        { this.h = h; }
+        //Get
+        public int GetBase()
+        { return this.b; }  
+        public int GetAltura()
+        { return this.h; }
+        public int GetArea()
+        { return this.a; }
+
+        //Calcular void
+        public void CalcularA()
+        { this.a = this.b * this.h; }
+    }
+}
---
+++ POO.Ex1/POO.Ex1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POO.Ex1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Calcular calcular = new Calcular(10,5);
+            calcular.CalcularA();
+            Console.WriteLine(calcular.GetArea());
+        }
+    }
+}
